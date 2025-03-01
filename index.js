11045c5b5809ceaf58ea909693bb206310f29f44
Thomas Tursics <thomas@tursics.de>
2025-03-01 16:31:58 +0100

    test

--- index.js
+++ index.js
@@ -1,65 +1,53 @@
-const MAPTILER_KEY = 'get_your_own_OpIi9ZULNHzrESv6T2vL';
+// https://basemap.de/data/produkte/web_vektor/anwendungsbeispiele/baudenkmale_brandenburg.html
+
 const map = new maplibregl.Map({
-//    style: `https://api.maptiler.com/maps/basic-v2/style.json?key=${MAPTILER_KEY}`,
-//    style: `https://api.maptiler.com/maps/basic-v2/style.json?key=${MAPTILER_KEY}`,
+    container: 'map',
+    style: 'https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/styles/bm_web_gry.json',
     center: [9.4333264, 54.7833021],
     zoom: 13,
-    pitch: 45,
+    pitch: 60,
     bearing: -17.6,
-    container: 'map',
     canvasContextAttributes: {antialias: true},
-    style: {
-        'version': 8,
-        'sources': {
-            'raster-tiles': {
-                'type': 'raster',
-                'tiles': [
-                    // NOTE: Layers from Stadia Maps do not require an API key for localhost development or most production
-                    // web deployments. See https://docs.stadiamaps.com/authentication/ for details.
-                    'https://tiles.oklabflensburg.de/fosm/{z}/{x}/{y}.png'
-//                    'https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg'
-                ],
-                'tileSize': 256,
-                'attribution': '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            }
-        },
-        'layers': [
-            {
-                'id': 'simple-tiles',
-                'type': 'raster',
-                'source': 'raster-tiles',
-                'minzoom': 0,
-                'maxzoom': 22
-            }
-        ]
-    }
 });
 
-// The 'building' layer in the streets vector source contains building-height
-// data from OpenStreetMap.
 map.on('load', () => {
     // Insert the layer beneath any symbol layer.
     const layers = map.getStyle().layers;
 
-    let labelLayerId;
+/*    let labelLayerId;
     for (let i = 0; i < layers.length; i++) {
         if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
             labelLayerId = layers[i].id;
             break;
         }
-    }
+    }*/
 
 /*    map.addSource('openmaptiles', {
         url: `https://api.maptiler.com/tiles/v3/tiles.json?key=${MAPTILER_KEY}`,
         type: 'vector',
     });*/
-    map.addSource('flensburgtiles', {
+/*    map.addSource('flensburgtiles', {
         tiles: [`https://tiles.oklabflensburg.de/fosm/{z}/{x}/{y}.png`],
         type: 'raster',
         attribution: 'Dingens',
-    });
+    });*/
         
-    map.addLayer(
+    // Add a layer showing the state polygons.
+    map.addLayer({
+        'id': 'Kulturdenkmale',
+        'type': "fill-extrusion",
+        'source': {
+            'type': 'geojson',
+//            'data': 'https://basemap.de/data/produkte/web_vektor/anwendungsbeispiele/geojson/baudenkmale_brandenburg.geojson'
+            'data': './stadt-flensburg.json'
+        },
+        'paint': {
+        'fill-extrusion-color': 'rgba(215, 59, 59, 1)',
+        'fill-extrusion-opacity': 0.8,
+        'fill-extrusion-height': 20}
+    });
+
+/*    map.addLayer(
         {
             'id': '3d-buildings',
 //            'source': 'openmaptiles',
@@ -90,5 +78,15 @@ map.on('load', () => {
             }
         },
         labelLayerId
-    );
+    );*/
 });
+
+// 54.7833021, 9.4333264
+
+// https://tiles.oklabflensburg.de/fosm/{z}/{x}/{y}.png
+
+// https://nominatim.oklabflensburg.de/search?q=norderstra%C3%9Fe%2049,%20flensburg
+// https://nominatim.oklabflensburg.de/search?q=norderstra%C3%9Fe%2049,%20flensburg&format=geocodejson
+// https://nominatim.oklabflensburg.de/search?q=norderstra%C3%9Fe%2049,%20flensburg&format=geocodejson&addressdetails=1
+
+// https://opendata.schleswig-holstein.de/dataset/cce3d31f-1a8a-4840-8fd7-986face6f229/resource/1aff4674-b6e7-4608-bec5-f81b3656eaf0
