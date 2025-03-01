5cca937cbe8e28c0791a87e4d631761835a87345
Thomas Tursics <thomas@tursics.de>
2025-03-01 20:18:23 +0100

    clean up project

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # Workshop Open Data Day 2025
 
-https://tursics.github.io/opendataday2025/
+Check out the result: https://tursics.github.io/opendataday2025/
 
-Show the Denkmalliste of Flensburg on a map
+## About the project
 
-- used https://opendata.schleswig-holstein.de/dataset/geodaten-denkmalliste-sh-2025-03-01
-- transform projection to WGS84
-- filter to Kreis Flensburg
+Am Internationalen Open Data Day 2025 am 1. März haben wir uns in Flensburg getroffen. Nach ein paar Inpulsvorträgen am Vormittag haben wir am Nachmittag Online-Karten vom Scratch gebaut. Ich habe die Idee der neuen Denkmalliste aus Schleswig-Holstein aufgegriffen und auf einer Karte visualisiert.
+
+## Implementation
+
+1. The basic structure of a map was set up with HTML, JS, CSS and a little boilerplate. I chose MapLibre GL JS because it allows you to use vector maps that can be zoomed, rotated, tilted as desired and thus also display 3D buildings.
+
+2. In order to display data on the maps, I looked for GeoJSON files in the Open Data Portal of Schleswig-Holstein, which also contain data from Flensburg. I found these: https://opendata.schleswig-holstein.de/dataset/geodaten-denkmalliste-sh-2025-03-01
+
+3. Unfortunately, the contents of the GeoJSON file are not displayed on the map. In QGIS I changed the projection from UTM 32 to WGS 84 and thus converted all coordinates.
+
+4. The resulting file was still too large to upload to GitHub. That's why I only filtered out the monuments in Flensburg in QGIS.
+
+5. Finished.
+
+The raster data map layer from OK Lab Flensburg (https://tiles.oklabflensburg.de/fosm/{z}/{x}/{y}.png) was presented and used in the workshop. I couldn't manage that and switched to the vector layer from Basemap.de.
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+// Based on this sample
 // https://basemap.de/data/produkte/web_vektor/anwendungsbeispiele/baudenkmale_brandenburg.html
 
 const map = new maplibregl.Map({
     container: 'map',
     style: 'https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/styles/bm_web_gry.json',
     center: [9.4333264, 54.7833021],
-//    center: [9.6655633813, 53.7559512825],
     zoom: 15,
     pitch: 60,
     bearing: -17.6,
@@ -12,40 +12,11 @@ const map = new maplibregl.Map({
 });
 
 map.on('load', () => {
-    // Insert the layer beneath any symbol layer.
-    const layers = map.getStyle().layers;
-
-/*    let labelLayerId;
-    for (let i = 0; i < layers.length; i++) {
-        if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
-            labelLayerId = layers[i].id;
-            break;
-        }
-    }*/
-
-/*    map.addSource('openmaptiles', {
-        url: `https://api.maptiler.com/tiles/v3/tiles.json?key=${MAPTILER_KEY}`,
-        type: 'vector',
-    });*/
-/*    map.addSource('flensburgtiles', {
-        tiles: [`https://tiles.oklabflensburg.de/fosm/{z}/{x}/{y}.png`],
-        type: 'raster',
-        attribution: 'Dingens',
-    });*/
-        
-    // Add a layer showing the state polygons.
     map.addLayer({
         'id': 'Kulturdenkmale',
-        'type': "fill-extrusion",
+        'type': 'fill-extrusion',
         'source': {
             'type': 'geojson',
-//            'data': 'https://basemap.de/data/produkte/web_vektor/anwendungsbeispiele/geojson/baudenkmale_brandenburg.geojson'
-//            'data': 'https://tursics.github.io/opendataday2025/stadt-flensburg-denkmalschutz.geojson'
-//            'data': 'https://tursics.github.io/opendataday2025/denkmalliste_geometrien.geojson'
-            // https://opendata.schleswig-holstein.de/dataset/b-plan-b-90
-//            'data': 'https://opendata.schleswig-holstein.de/data/elmshorn/Bebauungsplaene/B%2090/Elm_BP_090.json'
-            // https://opendata.schleswig-holstein.de/dataset/geodaten-denkmalliste-sh-2025-03-01
-//            'data': 'https://opendata.schleswig-holstein.de/dataset/2d2e094b-582a-4dcb-a217-875ad25f280b/resource/2d689e75-fbed-41fe-960c-fa11ddb4d837/download/geodaten-denkmalliste-sh.geojson'
             'data': 'https://raw.githubusercontent.com/tursics/opendataday2025/refs/heads/main/denkmalliste-flensburg.geojson'
         },
         'paint': {
@@ -53,48 +24,8 @@ map.on('load', () => {
         'fill-extrusion-opacity': 0.8,
         'fill-extrusion-height': 20}
     });
-
-/*    map.addLayer(
-        {
-            'id': '3d-buildings',
-//            'source': 'openmaptiles',
-//            'source': 'flensburgtiles',
-            'source-layer': 'building',
-            'type': 'fill-extrusion',
-            'minzoom': 15,
-            'filter': ['!=', ['get', 'hide_3d'], true],
-            'paint': {
-                'fill-extrusion-color': [
-                    'interpolate',
-                    ['linear'],
-                    ['get', 'render_height'], 0, 'lightgray', 200, 'royalblue', 400, 'lightblue'
-                ],
-                'fill-extrusion-height': [
-                    'interpolate',
-                    ['linear'],
-                    ['zoom'],
-                    15,
-                    0,
-                    16,
-                    ['get', 'render_height']
-                ],
-                'fill-extrusion-base': ['case',
-                    ['>=', ['get', 'zoom'], 16],
-                    ['get', 'render_min_height'], 0
-                ]
-            }
-        },
-        labelLayerId
-    );*/
 });
-
-// 54.7833021, 9.4333264
-
-// https://tiles.oklabflensburg.de/fosm/{z}/{x}/{y}.png
 
 // https://nominatim.oklabflensburg.de/search?q=norderstra%C3%9Fe%2049,%20flensburg
 // https://nominatim.oklabflensburg.de/search?q=norderstra%C3%9Fe%2049,%20flensburg&format=geocodejson
 // https://nominatim.oklabflensburg.de/search?q=norderstra%C3%9Fe%2049,%20flensburg&format=geocodejson&addressdetails=1
-
-// https://opendata.schleswig-holstein.de/dataset/cce3d31f-1a8a-4840-8fd7-986face6f229/resource/1aff4674-b6e7-4608-bec5-f81b3656eaf0
-// https://github.com/oklabflensburg/open-monuments-map/blob/main/data/stadt-flensburg-denkmalschutz.geojson
